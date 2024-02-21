9f01f45abd47db7d681451f92ab9abb2651c1b0f
tlswl7479 <tlswl7479@gmail.com>
2024-02-21 21:18:52 +0900

    .

---
+++ 13699.py
@@ -0,0 +1,15 @@
+def t(n):
+    if n == 0:
+        return 1
+    
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    
+    for i in range(1, n + 1):
+        for j in range(i):
+            dp[i] += dp[j] * dp[i - j - 1]
+    
+    return dp[n]
+
+n = int(input())
+print(t(n))
