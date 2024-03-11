8647097540e6ed161d0bdf65d1c99ba1f48e7912
tlswl7479 <tlswl7479@gmail.com>
2024-03-11 10:37:59 +0900

    .

---
+++ 1463.py
@@ -0,0 +1,18 @@
+x = int(input())
+
+# DP 테이블 초기화
+dp = [0] * (x + 1)
+
+for i in range(2, x + 1):
+    # 1을 빼는 경우
+    dp[i] = dp[i - 1] + 1
+
+    # 2로 나누어 떨어지는 경우
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+
+    # 3으로 나누어 떨어지는 경우
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
+print(dp[x])
